231036099d40eead6aa25594af4f62bc1469637f
root <root@hecs-x-medium-2-linux-20200829170927.(none)>
2020-10-17 16:09:24 +0800

    init

---
+++ api/api.go
@@ -0,0 +1,7 @@
+package api
+
+import "fmt"
+
+func HandleError() {
+    fmt.Println("api api.go Handle Error")
+}
---
+++ http.go
@@ -0,0 +1,8 @@
+package api
+
+import "fmt"
+
+func HandleReq() {
+    fmt.Println("api - http.go Handle Request")
+}
+
